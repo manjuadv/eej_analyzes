950e3e7264ffde83fe0e245e1bc1a3770a491f1f
Chamara Wijerathna <pathmakumaraad@gmail.com>
2019-11-17 10:13:46 +0900

    Working copy before outlier filters

---
+++ noonStudy/dataProcess.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def get_median_filtered(signal, threshold=3):
+    signal = signal.copy()
+    difference = np.abs(signal - np.median(signal))
+    median_difference = np.median(difference)
+    if median_difference == 0:
+        s = 0
+    else:
+        s = difference / float(median_difference)
+    mask = s > threshold
+    signal[mask] = np.median(signal)
+    return signal
